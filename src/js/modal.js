c4d9e4eba1fa3492d43da3536d50395ba7d3bacb
ThomasDev <thomas.jallu@gmail.com>
2021-11-13 18:04:21 +0100

    Ajout btn + modal
    Ajout des différents boutons (html)
    Ajout du modal de règles + son fonctionnement (html + js)

---
+++ src/js/modal.js
@@ -0,0 +1,25 @@
+// on récupère le modal dans le DOM
+const modal = document.getElementById("game-rules-modal");
+
+// on récupère le btn qui ouvre le modal
+const openModalBtn = document.getElementById("game-rules");
+
+// on récupère le btn pour close le modal
+const closeBtn = document.getElementsByClassName("close")[0];
+
+// Lorsque l'on clique sur le btn des règles on ouvre le modal
+openModalBtn.onclick = () => {
+  modal.style.display = "block";
+}
+
+// Lorsque l'on clique sur le croix du modal, on le ferme
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+}
+
+// Lorsque l'on clique n'importe où hors du modal, on le ferme
+window.onclick = (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
