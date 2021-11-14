76039ce7c15dc045b773486ddc0dbfb1b31c94f8
ThomasDev <thomas.jallu@gmail.com>
2021-11-14 01:57:30 +0100

    Couleur aléatoire
    Ajout des fonctions pour déclencher aléatoirement un bouton.
    En fonction de la suite aléatoire, on génère la suite de déclenchement avec une intervalle.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,87 @@
+// Array d'objets qui représentent les boutons
+const colorBtns = [
+    {
+        color: "green",
+        musicNote: "C4",
+    },
+    {
+        color: "red",
+        musicNote: "D4",
+    },
+    {
+        color: "yellow",
+        musicNote: "F4",
+    },
+    {
+        color: "blue",
+        musicNote: "E4",
+    }
+]
 
-btnInGame = document.querySelectorAll(".color-btn");
+const colorSeries = [];
 
-// Déclaration de du synthé pour le note de musique
-const synth = new Tone.Synth().toDestination();
-const now = Tone.now()
+const soundTime = 300;
+let interval = 800;
+
+const btnInGame = document.querySelectorAll(".color-btn");
+const startBtn = document.querySelector('.start-game-btn');
+
+const startDiv = document.querySelector('.start-div');
+
+startBtn.addEventListener('click', () => {
+    startBtn.style.display = 'none';
+    startDiv.style.display = 'block';
+    addColorBtnToArrayOfColorSeries()
+    playColorSeries(1000)
+})
 
 // A chaque clique sur un bouton de couleur, une note est joué 
 btnInGame.forEach(element => {
     element.addEventListener('click', (e) => {
-
         // On joue la note en fonction de la data attributes du bouton
-        synth.triggerAttackRelease(`${e.target.dataset.sound}`, now);
+        playSound(e.target.dataset.musicnote)
     })
 });
+
+function addColorBtnToArrayOfColorSeries() {
+    colorSeries.push(colorBtns[getRandomIndex()]);
+}
+
+function getRandomIndex() {
+    const randomIndex = parseInt(Math.random() * btnInGame.length);
+
+    return randomIndex;
+}
+
+function playSound(musicNote) {
+    // Déclaration de du synthé pour le note de musique
+    const synth = new Tone.Synth().toDestination();
+
+    synth.triggerAttackRelease(`${musicNote}`, 0.1);
+}
+
+function playClick(color) {
+    const colorBtn = document.querySelector(`.${color}-btn`);
+    colorBtn.classList.add(`${color}-btn-animated`);
+    setTimeout(() => {
+        colorBtn.classList.remove(`${color}-btn-animated`);
+    }, soundTime)
+}
+
+function playColorSeries(delay = 500) {
+    setTimeout(() => {
+        colorSeries.forEach((obj, index) => {
+            const delay = index * (soundTime + interval);
+            playSoundInColorSeries(obj.musicNote, obj.color, delay)
+            
+        })
+    }, delay)
+}
+
+function playSoundInColorSeries(musicNote, color, delay) {
+    setTimeout(() => {
+        playClick(color);
+        playSound(musicNote);
+    }, delay)
+    
+}
--- src/js/modal.js
+++ src/js/modal.js
@@ -9,17 +9,17 @@ const closeBtn = document.getElementsByClassName("close")[0];
 
 // Lorsque l'on clique sur le btn des règles on ouvre le modal
 openModalBtn.onclick = () => {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // Lorsque l'on clique sur le croix du modal, on le ferme
 closeBtn.onclick = () => {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // Lorsque l'on clique n'importe où hors du modal, on le ferme
 window.onclick = (e) => {
-  if (e.target == modal) {
-    modal.style.display = "none";
-  }
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
 }
